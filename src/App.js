f556597a1a9229377d59c349eca3fbe1da6a9f8a
gayathris99 <gayathri11499@gmail.com>
2021-04-21 19:04:37 +0530

    react-basics

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className = 'container'>
+      <Header title = "G3"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = (props) => {
+   return (
+       <div>
+           <h1 style = {{color : 'red'}}>Hello {props.title}</h1>
+       </div>
+   ) 
+}
+
+export default Header
